0b02dee3c98d8373c431b3218c0f9afbf4331ecb
heyethereum <heyethereum@gmail.com>
2022-10-09 19:46:53 +0800

    custom camera controller

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import { Canvas } from '@react-three/fiber';
 import AnimatedBox from '../components/AnimatedBox';
+import CameraOrbitController from '../components/CameraOrbitController';
 
 
 
@@ -8,6 +9,7 @@ const Home: NextPage = () => {
   return (
     <div className='container'>
       <Canvas>
+        <CameraOrbitController />
         <ambientLight intensity={0.1} />
         <directionalLight color="red" position={[0, 0, 5]} />
         <AnimatedBox />
